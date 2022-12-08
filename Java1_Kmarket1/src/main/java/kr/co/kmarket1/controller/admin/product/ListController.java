ff2b1f9deccbe51a5c763c641f492c7a223c60ba
khm9990503 <khm9990503@gmail.com>
2022-12-08 22:54:48 +0900

    2022/12/08 서버오류 수정-구홍모

--- Java1_Kmarket1/src/main/java/kr/co/kmarket1/controller/admin/product/ListController.java
+++ Java1_Kmarket1/src/main/java/kr/co/kmarket1/controller/admin/product/ListController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("")
+//@WebServlet("")
 public class ListController extends HttpServlet{
 private static final long serialVersionUID = 1L;
 	
