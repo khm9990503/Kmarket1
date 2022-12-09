777042945bb1c152c9c57b55a42159271517cc2e
khm9990503 <khm9990503@gmail.com>
2022-12-09 10:52:25 +0900

    2022/12/09 sql문 추가-구홍모

---
+++ Java1_Kmarket1/src/main/java/kr/co/kmarket1/db/SQL.java
@@ -0,0 +1,6 @@
+package kr.co.kmarket1.db;
+
+public class SQL {
+	// sql은 공용으로 사용하기에 수정하시거나 추가하신 sql문은 주석을 달아주세요~////////////////////
+	// select , insert , update , delete 순으로 뭉쳐서 작성해주면 보기 편해요.
+}
