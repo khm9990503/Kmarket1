3ddec57b7454891aec96b31c2fc655edaee5c9b2
khm9990503 <khm9990503@gmail.com>
2022-12-10 19:09:57 +0900

    2022/12/10 상품등록기능1차완료-구홍모

--- Java1_Kmarket1/src/main/java/kr/co/kmarket1/dao/ProductDao.java
+++ Java1_Kmarket1/src/main/java/kr/co/kmarket1/dao/ProductDao.java
@@ -22,35 +22,41 @@ public class ProductDao extends DBHelper{
 	public void selectProduct () {}
 	public void selectProducts () {}
 	// RegisterController doPost데이터베이스
-	public void insertProduct (ProductVO vo) {
+	public int insertProduct (ProductVO vo) {
+		int result = 0;
 		try {
 			logger.info("insertProduct...");
 			
 			conn = getConnection();
 			psmt = conn.prepareStatement(SQL.INSERT_PRODUCT);
-			psmt.setString(1, vo.getProdName());
-			psmt.setString(2, vo.getDescript());
-			psmt.setString(3, vo.getCompany());
-			psmt.setInt(4, vo.getPrice());
-			psmt.setInt(5, vo.getDiscount());
-			psmt.setInt(6, vo.getPoint());
-			psmt.setInt(7, vo.getStock());
-			psmt.setInt(8, vo.getDelivery());
-			psmt.setString(9, vo.getThumb1());
-			psmt.setString(10, vo.getThumb2());
-			psmt.setString(11, vo.getThumb3());
-			psmt.setString(12, vo.getDetail());
-			psmt.setString(13, vo.getStatus());
-			psmt.setString(14, vo.getDuty());
-			psmt.setString(15, vo.getReceipt());
-			psmt.setString(16, vo.getOrigin());
-			psmt.setString(17, vo.getIp());
-			psmt.executeUpdate();
+			psmt.setInt(1, vo.getProdCate1());
+			psmt.setInt(2, vo.getProdCate2());
+			psmt.setString(3, vo.getProdName());
+			psmt.setString(4, vo.getDescript());
+			psmt.setString(5, vo.getCompany());
+			psmt.setString(6, vo.getSeller());
+			psmt.setInt(7, vo.getPrice());
+			psmt.setInt(8, vo.getDiscount());
+			psmt.setInt(9, vo.getPoint());
+			psmt.setInt(10, vo.getStock());
+			psmt.setInt(11, vo.getDelivery());
+			psmt.setString(12, vo.getThumb1());
+			psmt.setString(13, vo.getThumb2());
+			psmt.setString(14, vo.getThumb3());
+			psmt.setString(15, vo.getDetail());
+			psmt.setString(16, vo.getStatus());
+			psmt.setString(17, vo.getDuty());
+			psmt.setString(18, vo.getReceipt());
+			psmt.setString(19, vo.getBizType());
+			psmt.setString(20, vo.getOrigin());
+			psmt.setString(21, vo.getIp());
+			result = psmt.executeUpdate();
 			close();
 			
 		}catch(Exception e) {
 			logger.error(e.getMessage());
 		}
+		return result;
 	}
 	public void updateProduct () {}
 	public void deleteProduct () {}
